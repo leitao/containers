6352d4952118a7f511cc73284b0a14a1559ab433
Breno Leitao <breno.leitao@gmail.com>
2016-02-18 12:04:22 -0500

    New fixes

--- docker/launch/docker_launch_a_debian_container.sh
+++ docker/launch/docker_launch_a_debian_container.sh
@@ -22,27 +22,28 @@ docker create --name=mycontainer ppc64le/debian
 docker ps -a
 
 # Start another container to run a command
-docker run ppc64le/debian ps aux
+docker run --name=mycontainer2 ppc64le/debian ps aux
 
 # Show the containers again
 docker ps -a
 
 # Create another container and change the image
-docker run --name=mycontainer2 ppc64le/debian mkdir /tmp/foo
+docker run --name=mycontainer3 ppc64le/debian mkdir /tmp/foo
 
 # Show the changes made by the container
-docker diff mycontainer2
+docker diff mycontainer3
 
 # Commit the changes to a new image
 docker commit -a "Author" -m "Add a net tmp directory" mycontainer2 debian_v2
 
 # Create a new container based on the new image and attach to it
-docker run -n mycontainer3 -i -t debian_v2 bash 
+docker run --name=mycontainer3 -i -t debian_v2 bash 
 
 # Remove the containers
 docker rm mycontainer
 docker rm mycontainer2
 docker rm mycontainer3
+docker rm mycontainer4
 
 # Remove the new image
 docker rmi debian_v2
