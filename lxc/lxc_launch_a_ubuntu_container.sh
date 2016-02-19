bcdaffae4e27d47c54bf0747b2eaf06b0a2d794f
Breno Leitao <breno.leitao@gmail.com>
2016-02-19 08:59:19 -0500

    New commands to show LXC commands

--- lxc/lxc_launch_a_ubuntu_container.sh
+++ lxc/lxc_launch_a_ubuntu_container.sh
@@ -13,5 +13,32 @@ lxc-create -n ubuntu01 -t ubuntu
 # Start the container
 lxc-start -n ubuntu01 -d
 
-# Get access to the container
+# list the containers in fancy (-f) mode. To list the IPs
+lxc-ls -f
+
+# Get access to the container. Ctrl-A q to leave the container
+# user is ubuntu and passwd is ubuntu
 lxc-console -n ubuntu01
+
+# lxc-freeze  freezes all the processes running inside the container. The
+# processes will be blocked until they are explicitly thawed by the
+# lxc-unfreeze command. This command is useful for batch managers to schedule a
+# group of processes.
+lxc-freeze -n ubuntu01
+
+# Show the containers freeze
+lxc-ls -f
+
+# Unfreeze the container
+lxc-unfreeze -n ubuntu01
+
+# Stop the container
+lxc-stop -n ubuntu01
+
+# Destroy the container
+lxc-destroy -n ubuntu01
+
+
+# Try to create another container using
+#lxc-create -n mycontainer -t download
+
