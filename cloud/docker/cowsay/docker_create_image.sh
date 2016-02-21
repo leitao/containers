538197b76567e32e47e0a31b909cd9525282ad3c
Breno Leitao <brenohl@br.ibm.com>
2016-02-21 19:58:41 -0300

    Fix to better handle remote build

--- cloud/docker/cowsay/docker_create_image.sh
+++ cloud/docker/cowsay/docker_create_image.sh
@@ -1,8 +1,9 @@
 #1/bin/bash
 
 IMAGE=cowimage
+DOCKERFILEPATH=/home/ubuntu/containers/cloud/docker/cowsay
 
-docker build -t $IMAGE .
+docker build -t $IMAGE $DOCKERFILEPATH
 
 # Run the image 
 docker run $IMAGE
